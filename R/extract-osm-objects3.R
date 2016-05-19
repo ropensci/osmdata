af03cbcd9884599e33727ad90b9a766c537856b2
mpadge <mark.padgham@email.com>
2016-05-19 22:32:58 +0200

    add R/extract-osm-objects3.R

---
+++ R/extract-osm-objects3.R
@@ -0,0 +1,60 @@
+#' get_xml_doc3
+#'
+#' Stripped from 'osmplotr/extract_osm_objects' to extract highways only for
+#' timing comparison.
+#'
+#' @param bbox the bounding box within which highways should be downloaded.  A
+#' 2-by-2 matrix of 4 elements with columns of min and max values, and rows of x
+#' and y values.
+#'
+#' @return An XML 'XMLAbstractDocument'
+#' @export
+
+get_xml_doc3 <- function (bbox=NULL)
+{
+    bbox <- paste0 ('(', bbox [2,1], ',', bbox [1,1], ',',
+                    bbox [2,2], ',', bbox [1,2], ')')
+
+    key <- "['highway']"
+    query <- paste0 ('(node', key, bbox,
+                    ';way', key, bbox,
+                    ';rel', key, bbox, ';')
+    url_base <- 'http://overpass-api.de/api/interpreter?data='
+    query <- paste0 (url_base, query, ');(._;>;);out;')
+
+    dat <- httr::GET (query)
+    if (dat$status_code != 200)
+        warning (httr::http_status (dat)$message)
+    # Encoding must be supplied to suppress warning
+    httr::content (dat, "text", encoding='UTF-8')
+}
+
+#' process_xml_doc3
+#'
+#' @param doc A text document extracted with 'get_xml_doc3'
+#'
+#' @return A SpatialLinesDataFrame
+#' @export
+#' 
+#' @examples
+#' bbox <- matrix (c (-0.13, 51.5, -0.11, 51.52), nrow=2, ncol=2)
+#' doc <- get_xml_doc (bbox=bbox)
+#' obj <- process_xml_doc (doc)
+
+process_xml_doc3 <- function (txt)
+{
+    dat <- get_highways (txt)
+    nd <- names (dat)
+    indx <- which (nchar (nd) == 0)
+    nd2 <- rep (1, length (indx))
+    while (any (duplicated (nd2)))
+        nd2 <- paste0 (round (runif (length (indx)) * 1e6))
+    nd [indx] <- nd2
+    for (i in seq (dat)) 
+    {
+        di <- data.frame (do.call (rbind,  dat [[i]]))
+        names (di) <- c ('x', 'y')
+        dat [[i]] <- sp::Lines (sp::Line (di), ID=nd [i])
+    }
+    sp::SpatialLines (dat)
+}
