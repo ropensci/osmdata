3bd4abfe155434d13b7a616bb854c2e111535b6a
mpadge <mark.padgham@email.com>
2016-10-10 14:00:51 +0200

    update testthat.R

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,8 +1,8 @@
 library(testthat)
-#library(osmdatar)
-wd <- getwd ()
-while (length (grep ('osmdatar', getwd ())) > 0) setwd ("..")
-devtools::load_all ('osmdatar', export_all=FALSE)
-setwd (wd)
+library(osmdatar)
+#wd <- getwd ()
+#while (length (grep ('osmdatar', getwd ())) > 0) setwd ("..")
+#devtools::load_all ('osmdatar', export_all=FALSE)
+#setwd (wd)
 
 test_check("osmdatar")
