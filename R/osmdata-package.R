d4cd60c22b6c7cfb80d481965728399d736248e5
mpadge <mark.padgham@email.com>
2016-11-29 14:33:12 +0100

    change import lubridate -> importFrom lubridate

--- R/osmdata-package.R
+++ R/osmdata-package.R
@@ -32,13 +32,14 @@
 #' @name osmdata
 #' @docType package
 #' @author Bob Rudis, Robin Lovelace, Maëlle Salmon, Mark Padgham
-#' @import sp httr lubridate
+#' @import sp httr 
 #' @importFrom rvest html_attr html_nodes
 #' @importFrom xml2 read_html read_xml  xml_attr
 #' @importFrom dplyr %>% select left_join filter arrange bind_rows mutate do group_by distinct data_frame
 #' @importFrom utils read.table timestamp
 #' @importFrom curl has_internet
 #' @importFrom Rcpp evalCpp
+#' @importFrom lubridate ymd_hms force_tz
 #' @useDynLib osmdata
 NULL
 
