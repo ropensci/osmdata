e38e29347b51e9b9f5fb4e8b68dd0176b60c35c8
Robin Lovelace <rob00x@gmail.com>
2016-12-26 13:00:40 +0000

    Add prototype test/mock code (WIP)

--- tests/testthat/test-getbb.R
+++ tests/testthat/test-getbb.R
@@ -4,51 +4,12 @@ is_travis <-  identical (Sys.getenv("TRAVIS"), "true")
 
 get_local <- FALSE
 if (get_local) {
-  qry <- getbb("Salzburg")
-  qry <- add_feature (qry, key = 'highway', value = 'tertiary')
-  qry <- paste0 (c (qry$features, qry$suffix), collapse = "\n")
-  
-  cfm_output_overpass_query <- NULL
-  trace(
-    curl::curl_fetch_memory,
-    exit = function() { cfm_output_overpass_query <<- returnValue() }
-  )
-  res <- httr::POST (base_url, body=qry)
-  #names (cfm_output_make_query)
-  untrace (curl::curl_fetch_memory)
-  save (cfm_output_overpass_query, 
-        file="./tests/testthat/cfm_output_overpass_query.rda")
+  bb_test <- getbb("Salzburg")
+  saveRDS (bb_test, 
+           file = "./tests/testthat/bb_test.Rds")
 }
-
-context ("overpass query")
-test_that ("overpass null values", {
-  expect_error (overpass_query (), "query must be supplied")
-  expect_error (overpass_query (1), 
-                "query must contain nothing but character strings")
-})
-
-test_that ("make_query", {
-  qry <- opq (bbox=c(-0.12,51.11,-0.11,51.12)) 
-  qry <- add_feature (qry, key='highway', value='tertiary')
   
-  if (!has_internet) {
-    expect_message (overpass_query (qry), 
-                    "Overpass query unavailable without internet", call.=FALSE)
-  } else 
-  {
-    if (is_cran)
-    {
-      load("cfm_output_overpass_query.rda")
-      stub (overpass_query, 'httr::POST', 
-            function (x, ...) cfm_output_overpass_query$content )
-    } 
-    # NOTE: This still issues a query for overpass_status
-    res <- osmdata_xml (qry)
-    expect_true (is (res, "xml_document"))
-    res <- osmdata_sp (qry)
-    expect_s3_class (res, "osmdata")
-    expect_named (res, expected=c ("bbox", "overpass_call", "timestamp",
-                                   "osm_points", "osm_lines", "osm_polygons"),
-                  ignore.order=FALSE)
+  if (has_internet) {
+    f <- "tests/testthat/bb_test.Rds"
+    expect_equal(getbb("Salzburg"), readRDS(f))
   }
-})
