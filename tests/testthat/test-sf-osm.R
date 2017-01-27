4dddaca79c3c93ec8811eb9171f708909c945fe4
mpadge <mark.padgham@email.com>
2017-01-27 12:46:38 +0100

    add testthat/test-sf-osm.R

---
+++ tests/testthat/test-sf-osm.R
@@ -0,0 +1,42 @@
+context ("sf-osm")
+
+test_that ("multipolygon", {
+               x_sf <- sf::st_read ("../osm-multi.osm", layer="multipolygons", 
+                                    quiet=TRUE)
+               q0 <- opq (bbox=c(1,1,5,5)) 
+               x <- osmdata_sf (q0, "../osm-multi.osm")$osm_multipolygons
+               # GDAL spits out a whole lot of generic field names, so first the
+               # two have to be reduced to common fields.  
+               x <- x [, which (names (x) %in% names (x_sf))]
+               x_sf <- x_sf [, which (names (x_sf) %in% names (x))]
+               # Then all of the object and dimnames inserted by `osmdata` have
+               # to be removed
+               rownames (x_sf) <- rownames (x)
+               for (i in seq (length (x$geometry [[1]])))
+               {
+                   names (x$geometry [[1]] [[i]]) <- NULL
+                   for (j in seq (length (x$geometry [[1]] [[i]])))
+                       dimnames (x$geometry [[1]] [[i]] [[j]]) <- NULL
+               }
+               names (x$geometry) <- NULL
+               expect_identical (x, x_sf)
+})
+
+
+test_that ("multilinestring", {
+               x_sf <- sf::st_read ("../osm-multi.osm", layer="multilinestrings", 
+                                    quiet=TRUE)
+               q0 <- opq (bbox=c(1,1,5,5)) 
+               x <- osmdata_sf (q0, "../osm-multi.osm")$osm_multilines
+               x <- x [, which (names (x) %in% names (x_sf))]
+               x_sf <- x_sf [, which (names (x_sf) %in% names (x))]
+               rownames (x_sf) <- rownames (x)
+               names (x$geometry [[1]]) <- NULL
+               for (i in seq (length (x$geometry [[1]])))
+               {
+                   names (x$geometry [[1]] [[i]]) <- NULL
+                   dimnames (x$geometry [[1]] [[i]]) <- NULL
+               }
+               names (x$geometry) <- NULL
+               expect_identical (x, x_sf)
+})
