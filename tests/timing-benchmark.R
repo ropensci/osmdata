02f30fd8f4f7f41e0174df433fa1630cddf329b2
mpadge <mark.padgham@email.com>
2017-02-01 18:20:40 +0100

    update tests/timing-benchmark.R

--- tests/timing-benchmark.R
+++ tests/timing-benchmark.R
@@ -1,7 +1,7 @@
 #library (osmdata)
 # NOTE: Currently requires devtools::load_all ("osmdata", export_all=TRUE)!
 
-benchmark <- function ()
+benchmark <- function (times=100)
 {
     devtools::load_all (".", export_all=FALSE)
     q0 <- opq (bbox=c(-0.27,51.47,-0.20,51.50)) 
@@ -9,18 +9,14 @@ benchmark <- function ()
     # contains both multipolygons and multilinestrings
     doc <- osmdata_xml (q1, "export.osm")
 
-    #q0 <- opq (bbox=c(-0.12,51.51,-0.11,51.52)) # Central London, U.K.
-    #q1 <- add_feature (q0, key='building')
-    #query <- paste0 (c (q1$features, q1$suffix), collapse="\n")
-    #osmdata_xml (query, "export.osm")
-
     objs <- c ("points", "lines", "multilinestrings", "multipolygons",
                "other_relations")
     mt_sf <- size_sf <- NULL
     for (i in seq (objs))
     {
         mb <- microbenchmark::microbenchmark (
-           dat <- sf::st_read ("export.osm", layer=objs [i], quiet=TRUE) , times=10L)
+           dat <- sf::st_read ("export.osm", layer=objs [i], quiet=TRUE) , 
+           times=times)
         size_sf <- c (size_sf, object.size (dat))
         mt_sf <- c (mt_sf, median (mb$time))
         cat ("\r", i, " / ", length (objs))
@@ -29,12 +25,13 @@ benchmark <- function ()
     cat ("\rSF: Median times (in ms) for (", paste (objs), "):\n")
     cat ("\t(", mt_sf, "); total = ", sum (mt_sf), "\n")
 
-    mb <- microbenchmark::microbenchmark ( x <- osmdata_sf (q1, doc), times=10L)
+    mb <- microbenchmark::microbenchmark ( x <- osmdata_sf (q1, doc), 
+                                          times=times)
     #mb <- microbenchmark::microbenchmark ( x <- osmdata_sf (q1, "export.osm"),
     #                                      times=10L)
     mt <- median (mb$time / 1e6)
     cat ("osmdata: Median time = ", mt, " ms\n")
-    size_od <- object.size (dat)
+    size_od <- object.size (x)
 
     cat ("\nosmdata took ", mt / sum (mt_sf), " times longer to extract ",
          size_od / sum (size_sf), " times as much data\n")
