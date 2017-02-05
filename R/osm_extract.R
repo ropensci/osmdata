17794c7485c6559ba2db9adf8ad02eddb9d0b760
mpadge <mark.padgham@email.com>
2017-02-05 20:50:40 +0100

    add osm_multilines/multipolygons functions

--- R/osm_extract.R
+++ R/osm_extract.R
@@ -144,3 +144,112 @@ osm_polygons <- function(dat, id) {
     dat$osm_polygons [which (rownames (dat$osm_polygons) %in% ids), ]
 }
 
+
+#' Extract all \code{osm_multilines} from an osmdata object
+#'
+#' \code{id} must be of an \code{osm_point} or \code{osm_line} object (and can
+#' not be the \code{id} of an \code{osm_polygon} object because multilines by
+#' definition contain no polygons.  \code{osm_multilines} returns any multiline
+#' object(s) which contain the object specified by \code{id}.
+#'
+#'
+#' @param dat An object of class \code{osmdata}
+#' @param id OMS identification of object for which multilines are to be extracted
+#' @return An \code{sf} Simple Features Collection of multilines 
+#'
+#' @export
+osm_multilines <- function(dat, id) {
+    if (missing (dat))
+        stop ('osm_multilines can not be extracted without data')
+    if (!is (dat, 'osmdata'))
+        stop ('dat must be of class osmdata')
+    if (missing (id))
+        stop ('id must be given to extract multilines')
+    if (!(is.character (id) | is.numeric (id)))
+        stop ('id must be of class character or numeric')
+
+    if (!is.character (id))
+        id <- as.character (id)
+
+    indx <- which (grepl ('osm_', names (dat)))
+    where <- indx [which (sapply (dat [indx], function (i) id %in% rownames (i)))]
+    x <- dat [[where]] [which (rownames (dat [[where]]) == id),]$geometry
+
+    if (is (x, 'sfc_POLYGON'))
+        stop ('multilines do not contain polygons')
+    else if (any (grepl ('MULTI', class (x))))
+        stop ('osm_multilines id must be of a point or line object')
+
+    if (is (x, 'sfc_POINT'))
+    { 
+        # find all lines containing that point
+        indx <- which (sapply (dat$osm_lines$geometry, function (i) 
+                               any (rownames (i) %in% id)))
+        ids <- names (indx)
+        # then find all multilines containing those lines
+        indx <- which (sapply (dat$osm_multilines$geometry, function (i) 
+                               any (names (i) %in% ids)))
+    } else if (is (x, 'sfc_LINESTRING'))
+        indx <- which (sapply (dat$osm_multilines$geometry, function (i) 
+                               any (names (i) %in% id)))
+
+    dat$osm_multilines [indx, ]
+}
+
+#' Extract all \code{osm_multipolygons} from an osmdata object
+#'
+#' \code{id} must be of an \code{osm_point}, \code{osm_line}, or
+#' \code{osm_polygon} object. \code{osm_multipolygons} returns any multipolygon
+#' object(s) which contain the object specified by \code{id}.
+#'
+#'
+#' @param dat An object of class \code{osmdata}
+#' @param id OMS identification of object for which multipolygons are to be extracted
+#' @return An \code{sf} Simple Features Collection of multipolygons 
+#'
+#' @export
+osm_multipolygons <- function(dat, id) {
+    if (missing (dat))
+        stop ('osm_multipolygons can not be extracted without data')
+    if (!is (dat, 'osmdata'))
+        stop ('dat must be of class osmdata')
+    if (missing (id))
+        stop ('id must be given to extract multipolygons')
+    if (!(is.character (id) | is.numeric (id)))
+        stop ('id must be of class character or numeric')
+
+    if (!is.character (id))
+        id <- as.character (id)
+
+    indx <- which (grepl ('osm_', names (dat)))
+    where <- indx [which (sapply (dat [indx], function (i) id %in% rownames (i)))]
+    x <- dat [[where]] [which (rownames (dat [[where]]) == id),]$geometry
+
+    if (any (grepl ('MULTI', class (x))))
+        stop ('osm_multipolygons id must be of a point, line, or polygon object')
+
+    if (is (x, 'sfc_POINT'))
+    { 
+        # find all lines containing that point
+        indx <- which (sapply (dat$osm_lines$geometry, function (i) 
+                               any (rownames (i) %in% id)))
+        ids <- names (indx)
+        indx <- which (sapply (dat$osm_polygons$geometry, function (i) 
+                               any (rownames (i [[1]]) %in% id)))
+        ids <- c (ids, names (indx))
+        # then find all multipolygons containing those lines
+        indx <- which (sapply (dat$osm_multipolygons$geometry, function (i) 
+                               {
+                                   ids_i <- unlist (sapply (names (i [[1]]), 
+                                                            function (j) 
+                                                                strsplit (j, '-')))
+                                   names (ids_i) <- NULL
+                                   any (ids_i %in% ids)
+                               }))
+    } else if (class (x) [1] %in% c ('sfc_LINESTRING', 'sfc_POLYGON'))
+        indx <- which (sapply (dat$osm_multipolygons$geometry, function (i) 
+                               any (names (i [[1]]) %in% id)))
+
+    dat$osm_multipolygons [indx, ]
+}
+
