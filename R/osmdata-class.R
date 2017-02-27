a5aae9d716da764a1ecafcfcdc865701bb4f4031
mpadge <mark.padgham@email.com>
2017-02-27 16:27:22 +0100

    implement c method for osmdata class

--- R/osmdata-class.R
+++ R/osmdata-class.R
@@ -108,3 +108,80 @@ print.osmdata <- function (x, ...)
     #invisible (x)
 }
 
+#' @export
+c.osmdata <- function (...)
+{
+    x <- list (...)
+    cl_sf <- sapply (x, function (i) any (grep ('sf', sapply (i, class))))
+    if (!(all (cl_sf) | all (!cl_sf)))
+        stop ('All objects must be either osmdata_sf or osmdata_sp')
+    
+    sf <- all (cl_sf)
+    res <- osmdata ()
+    res$bbox <- x [[1]]$bbox
+    res$overpass_call <- x [[1]]$overpass_call
+    res$timestamp <- x [[1]]$timestamp
+
+    if (sf)
+    {
+        osm_indx <- which (grepl ('osm_', names (x [[1]])))
+        core_names <- c ('osm_id', 'name', 'geometry')
+        for (i in osm_indx)
+        {
+            xi <- lapply (x, function (j) j [[i]])
+            indx <- which (sapply (xi, nrow) > 0)
+            xi <- xi [indx]
+            if (length (xi) > 0)
+            {
+                ids <- cnames <- NULL
+                for (j in xi) 
+                {
+                    ids <- c (ids, rownames (j))
+                    cnames <- c (cnames, colnames (j))
+                }
+                ids <- sort (unique (ids))
+                cnames <- cnames [!cnames %in% core_names]
+                cnames <- sort (unique (cnames))
+                cnames <- c ('osm_id', 'name', cnames, 'geometry')
+                resi <- xi [[1]] 
+                # then expand resi to final number of columns keeping sf integrity 
+                cnames_new <- cnames [which (!cnames %in% names (resi))]
+                for (j in cnames_new)
+                    resi [j] <- rep (NA, nrow (resi))
+                # and re-order columns again
+                indx1 <- which (names (resi) %in% core_names)
+                indx2 <- which (!seq (ncol (resi)) %in% indx1)
+                indx <- c (which (names (resi) == 'osm_id'),
+                           which (names (resi) == 'name'),
+                           indx2 [order (names (resi) [indx2])],
+                           which (names (resi) == 'geometry'))
+                resi <- resi [,indx]
+                # Then we're finally ready to pack in the remaining bits
+                xi [[1]] <- NULL
+                for (j in xi)
+                {
+                    rindx <- which (!rownames (j) %in% rownames (resi))
+                    cindx <- which (names (j) %in% names (resi))
+                    resj <- j [rindx, ]
+                    # then expand resj as for resi above
+                    cnames_new <- cnames [which (!cnames %in% names (resj))]
+                    for (k in cnames_new)
+                        resj [k] <- rep (NA, nrow (resj))
+                    indx1 <- which (names (resj) %in% core_names)
+                    indx2 <- which (!seq (ncol (resj)) %in% indx1)
+                    indx <- c (which (names (resj) == 'osm_id'),
+                               which (names (resj) == 'name'),
+                               indx2 [order (names (resj) [indx2])],
+                               which (names (resj) == 'geometry'))
+                    resj <- resj [,indx]
+                    resi <- rbind (resi, resj)
+                } # end for j in x
+                res [[i]] <- resi
+            } # end if length (xi) > 0
+        } # end for i in osm_indx
+    } else
+    {
+        # TODO: implement sp version
+    }
+    return (res)
+}
