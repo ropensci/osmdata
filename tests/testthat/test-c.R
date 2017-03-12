cea6c4be7a38d13b79074b4e53802b2df7de949b
mpadge <mark.padgham@email.com>
2017-03-12 12:19:16 +0100

    add testthat/test-c.R

---
+++ tests/testthat/test-c.R
@@ -0,0 +1,15 @@
+context ("c")
+
+test_that ("c-method", {
+               q <- opq (bbox=c(1,1,5,5)) 
+               x1 <- osmdata_sf (q, "../osm-multi.osm")
+               x2 <- osmdata_sf (q, "../osm-ways.osm")
+               x <- c (x1, x2)
+               osm_indx <- which (grepl ('osm_', names (x)))
+               for (i in osm_indx)
+               {
+                   expect_true (nrow (x [[i]]) >= nrow (x1 [[i]]))
+                   expect_true (nrow (x [[i]]) >= nrow (x2 [[i]]))
+               }
+})
+
