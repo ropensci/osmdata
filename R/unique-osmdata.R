a5760b4807ad457c82996dee3e7f2b7e0f4615be
mpadge <mark.padgham@email.com>
2017-08-10 12:33:45 +0200

    implement unique-osmdata for sp as well as sf

--- R/unique-osmdata.R
+++ R/unique-osmdata.R
@@ -16,26 +16,29 @@ unique_osmdata <- function (dat)
     if (!is (dat, 'osmdata'))
         stop ('dat must be an osmdata object')
 
-    if (!is (dat$osm_points, 'sf'))
-        stop (paste0 ('unique_osmdata only currently implemented for ',
-                      'sf objects;\nplease convert sp to sf via ',
-                      'sf::as (., "Spatial")'))
+    if (is (dat$osm_points, 'sf'))
+    {
+        indx_points <- unique_points_sf (dat)
+        indx_lines <- unique_lines_sf (dat)
+        indx_polys <- unique_polygons_sf (dat)
+    } else
+    {
+        indx_points <- unique_points_sp (dat)
+        indx_lines <- unique_lines_sp (dat)
+        indx_polys <- unique_polygons_sp (dat)
+    }
 
-    indx_pts <- unique_pts (dat)
-    indx_lns <- unique_lines (dat)
-    indx_poly <- unique_polygons (dat)
-
-    dat$osm_points <- dat$osm_points [unique_points (dat), ]
-    dat$osm_lines <- dat$osm_lines [unique_lines (dat), ]
-    dat$osm_polygons <- dat$osm_polygons [unique_polygons (dat), ]
+    dat$osm_points <- dat$osm_points [indx_points, ]
+    dat$osm_lines <- dat$osm_lines [indx_lines, ]
+    dat$osm_polygons <- dat$osm_polygons [indx_polys, ]
 
     return (dat)
 }
 
-#' unique_points
+#' unique_points_sf
 #' get index of unique points in the \code{$osm_points} object
 #' @noRd
-unique_points <- function (dat)
+unique_points_sf <- function (dat)
 {
     pts <- paste0 (dat$osm_points$osm_id)
 
@@ -52,10 +55,34 @@ unique_points <- function (dat)
     which (!pts %in% c (lns_pts, poly_pts))
 }
 
-#' unique_lines
+#' unique_points_sp
+#' get index of unique points in the \code{$osm_points} object
+#' @noRd
+unique_points_sp <- function (dat)
+{
+    pts <- rownames (slot (dat$osm_points, "data"))
+
+    lns <- slot (dat$osm_lines, "lines")
+    lns_pts <- lapply (lns, function (i)
+                       rownames (slot (slot (i, "Lines") [[1]], "coords")))
+    lns_pts <- unlist (lns_pts)
+    names (lns_pts) <- NULL
+    lns_pts <- unique (lns_pts)
+
+    polys <- slot (dat$osm_polygons, "polygons")
+    poly_pts <- lapply (polys, function (i)
+                        rownames (slot (slot (i, "Polygons") [[1]], "coords")))
+    poly_pts <- unlist (poly_pts)
+    names (poly_pts) <- NULL
+    poly_pts <- unique (poly_pts)
+
+    which (!pts %in% c (lns_pts, poly_pts))
+}
+
+#' unique_lines_sf
 #' get index of unique lines in the \code{$osm_lines} object
 #' @noRd
-unique_lines <- function (dat)
+unique_lines_sf <- function (dat)
 {
     lns <- paste0 (dat$osm_lines$osm_id)
 
@@ -66,15 +93,45 @@ unique_lines <- function (dat)
     which (!lns %in% mlns)
 }
 
-#' unique_polygons
+#' unique_lines_sp
+#' get index of unique lines in the \code{$osm_lines} object
+#' @noRd
+unique_lines_sp <- function (dat)
+{
+    lns <- rownames (slot (dat$osm_lines, "data"))
+
+    mlns <- slot (dat$osm_multilines, "lines") [[1]]
+    mlns <- names (slot (mlns, "Lines"))
+    mlns <- unique (mlns)
+
+    which (!lns %in% mlns)
+}
+
+#' unique_polygons_sf
 #' get index of unique polygons in the \code{$osm_polygons} object
 #' @noRd
-unique_polygons <- function (dat)
+unique_polygons_sf <- function (dat)
 {
     polys <- paste0 (dat$osm_polygons$osm_id)
 
     mpolys <- unlist (lapply (dat$osm_multipolygons$geometry, function (i)
                               names (i [[1]])))
+    names (mpolys) <- NULL
+    mpolys <- unique (mpolys)
+
+    which (!polys %in% mpolys)
+}
+
+#' unique_polygons_sp
+#' get index of unique polygons in the \code{$osm_polygons} object
+#' @noRd
+unique_polygons_sp <- function (dat)
+{
+    polys <- rownames (slot (dat$osm_polygons, "data"))
+
+    mpolys <- slot (dat$osm_multipolygons, "polygons")
+    mpolys <- unlist (lapply (mpolys, function (i)
+                              names (slot (i, "Polygons"))))
     names (mpolys) <- NULL
     mpolys <- unique (mpolys)
 
