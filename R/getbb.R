0f46eb43ffea558df0fd96a75f711a15008f6116
Robin Lovelace <rob00x@gmail.com>
2017-12-18 22:13:36 +0000

    Update docs for getbb()

--- R/getbb.R
+++ R/getbb.R
@@ -129,9 +129,8 @@ bbox_to_string <- function(bbox) {
 #' bbmat = matrix(unlist(bb), ncol = 2)
 #' plot(bbmat) # worldwide coverage of places matching "london uk"
 #' bb_sf = getbb("kathmandu", format_out = "sf_polygon") 
-#' sf:::plot.sf(bb_sf)
-#' bb_sf = getbb("london", format_out = "sf_polygon") # only selects 1st of multipolygons
-#' getbb("kathmandu", format_out = "sf_polygon") 
+#' # sf:::plot.sf(bb_sf) # can be plotted if sf is installed
+#' getbb("london", format_out = "sf_polygon") # only selects 1st of multipolygons
 #' # Using an alternative service (locationiq requires an API key)
 #' key <- Sys.getenv("LOCATIONIQ") # add LOCATIONIQ=type_your_api_key_here to .Renviron
 #' if(nchar(key) ==  32) {
@@ -264,7 +263,6 @@ getbb <- function(place_name,
     }
     
     if(format_out == "sf_polygon") {
-      browser()
       if(is(ret[[poly_num[1]]], "matrix")) {
         ret = mat2sf_poly(ret[[poly_num[1]]], pname = place_name)
       } else {
