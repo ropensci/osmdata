fe7df5fd040212dec0287b8cb92a23e8d64401ae
mpadge <mark.padgham@email.com>
2018-11-27 11:28:59 +0100

    expand test-trim

--- tests/testthat/test-trim.R
+++ tests/testthat/test-trim.R
@@ -1,14 +1,30 @@
 context ("trim-osm-data")
 
-require (sf)
 
 test_that ('trim_osm_data', {
                q0 <- opq (bbox = c(1, 1, 5, 5))
                x0 <- osmdata_sf (q0, "../osm-multi.osm")
-               x1 <- trim_osmdata (x0, bb_poly = cbind (c (2, 3), c (2, 3)))
+               expect_message (x1 <- trim_osmdata (x0,
+                                    bb_poly = cbind (c (2, 3), c (2, 3))),
+                       "It is generally necessary to pre-load the sf package")
+               require (sf)
+               expect_silent (x1 <- trim_osmdata (x0,
+                                    bb_poly = cbind (c (2, 3), c (2, 3))))
+
                expect_identical (x0, x1)
 
-               x0 <- osmdata_sf (q0, "../osm-ways.osm")
-               x1 <- trim_osmdata (x0, bb_poly = cbind (c (2, 3), c (2, 3)))
+               bb <- list (cbind (c (0, 0),
+                                  c (0, 1),
+                                  c (1, 1),
+                                  c (1, 0),
+                                  c (0, 0)),
+                           cbind (c (2, 3),
+                                  c (2, 4),
+                                  c (3, 4),
+                                  c (3, 3),
+                                  c (2, 3)))
+
+               expect_message (x1 <- trim_osmdata (x0, bb_poly = bb),
+                               "bb_poly has more than one polygon")
                expect_identical (x0, x1)
 })
