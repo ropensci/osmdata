c4ed1fd29bec7c12ba63adbcb1795408d2dc86b5
mpadge <mark.padgham@email.com>
2018-11-27 10:35:45 +0100

    fix tests

--- tests/testthat/test-trim.R
+++ tests/testthat/test-trim.R
@@ -3,14 +3,12 @@ context ("trim-osm-data")
 require (sf)
 
 test_that ('trim_osm_data', {
+               q0 <- opq (bbox = c(1, 1, 5, 5))
                x0 <- osmdata_sf (q0, "../osm-multi.osm")
-               x1 <- trim_osmdata (x, bb_poly = cbind (c (2, 3), c (2, 3)))
-               expect_true (!identical (x0, x1))
-               # trim converts the polygon to a linestring, but for the ways,
-               # all objects extend through the trim bb_poly and out, so nothing
-               # is trimmed.
+               x1 <- trim_osmdata (x0, bb_poly = cbind (c (2, 3), c (2, 3)))
+               expect_identical (x0, x1)
 
                x0 <- osmdata_sf (q0, "../osm-ways.osm")
-               x1 <- trim_osmdata (x, bb_poly = cbind (c (2, 3), c (2, 3)))
+               x1 <- trim_osmdata (x0, bb_poly = cbind (c (2, 3), c (2, 3)))
                expect_identical (x0, x1)
 })
--- tests/testthat/test-unique.R
+++ tests/testthat/test-unique.R
@@ -3,8 +3,9 @@ context ("unique-osm-data")
 require (sf)
 
 test_that ('unique_osm_data', {
+               q0 <- opq (bbox = c(1, 1, 5, 5))
                x0 <- osmdata_sf (q0, "../osm-multi.osm")
-               x1 <- unique_osmdata (x)
+               x1 <- unique_osmdata (x0)
                expect_true (!identical (x0, x1))
                expect_true (nrow (x0$osm_points) > nrow (x1$osm_points))
                # trim converts the polygon to a linestring, but for the ways,
@@ -12,7 +13,7 @@ test_that ('unique_osm_data', {
                # is trimmed.
 
                x0 <- osmdata_sf (q0, "../osm-ways.osm")
-               x1 <- unique_osmdata (x)
+               x1 <- unique_osmdata (x0)
                expect_true (!identical (x0, x1))
                expect_true (nrow (x0$osm_points) > nrow (x1$osm_points))
 })
