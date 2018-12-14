b158fe8ceedb28f955e7a214619800d48fcef32e
mpadge <mark.padgham@email.com>
2018-12-14 11:11:02 +0100

    fix test/osmdata-sc

--- tests/testthat/test-sc-osm.R
+++ tests/testthat/test-sc-osm.R
@@ -5,12 +5,16 @@ test_all <- (identical (Sys.getenv ("MPADGE_LOCAL"), "true") |
              identical (Sys.getenv ("TRAVIS"), "true"))
              #identical (Sys.getenv ("APPVEYOR"), "True"))
 
+# Current names of SC objects as returned by osmdata:
+sc_names <- c ("nodes", "relation_members", "relation_properties",
+               "object", "object_link_edge", "edge", "vertex", "meta")
+
 test_that ("multipolygon", {
                q0 <- opq (bbox = c(1, 1, 5, 5))
                x <- osmdata_sc (q0, "../osm-multi.osm")
                # TODO: Write proper tests
                expect_is (x, "SC")
-               expect_equal (names (x), c ("object", "object_link_edge", "edge", "vertex", "meta"))
+               expect_equal (names (x), sc_names)
 })
 
 
@@ -18,5 +22,5 @@ test_that ("ways", {
                q0 <- opq (bbox = c(1, 1, 5, 5))
                x <- osmdata_sc (q0, "../osm-ways.osm")
                expect_is (x, "SC")
-               expect_equal (names (x), c ("object", "object_link_edge", "edge", "vertex", "meta"))
+               expect_equal (names (x), sc_names)
 })
