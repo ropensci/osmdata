91b102cf9f6883d8cb13ac1b86134381a4d7d9c6
mpadge <mark.padgham@email.com>
2018-12-29 22:14:17 +0100

    closes #162

--- R/osmdata-methods.R
+++ R/osmdata-methods.R
@@ -71,6 +71,15 @@ print.osmdata <- function (x, ...)
 c.osmdata <- function (...)
 {
     x <- list (...)
+    cl_null <- vapply (x, function (i)
+                       is.null (i$osm_points) &
+                           is.null (i$osm_lines) &
+                           is.null (i$osm_polygons) &
+                           is.null (i$osm_multilines) &
+                           is.null (i$osm_multipolygons),
+                       FUN.VALUE = logical (1))
+    if (any (cl_null))
+        stop ("osmdata object is entirely NULL")
     cl_sf <- vapply (x, function (i)
                      any (grep ('sf', lapply (i, class))),
                      FUN.VALUE = logical (1))
