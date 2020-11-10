50eb76bf70d5e7457631c3cd7e63f56d10200a77
idshklein <idshklein@gmail.com>
2020-11-10 15:45:10 +0200

    fix null lat lon bug

--- R/opq.R
+++ R/opq.R
@@ -390,8 +390,8 @@ opq_string_intern <- function (opq, quiet = TRUE)
                                          features,
                                          opq$bbox))
         else if (!is.null (attr (opq, "enclosing")))
-            features <- paste0 ("is_in(", lat, ",", lon, ")->.a;",
-                                attr (opq, "enclosing"), "(pivot.a)",
+            features <- paste0("is_in(", strsplit(opq$bbox,",")[[1]][1], ",", 
+                       strsplit(opq$bbox,",")[[1]][2], ")->.a;", attr(opq, "enclosing"), 
                                 features,
                                 ";")
         else
