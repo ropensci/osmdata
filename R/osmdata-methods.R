9f403827840b31061f72d6c409a32c981942bf74
mpadge <mark.padgham@email.com>
2021-01-12 11:09:03 +0100

    separate c_sf function for c.osmdata method

--- R/osmdata-methods.R
+++ R/osmdata-methods.R
@@ -107,71 +107,7 @@ c.osmdata <- function (...)
 
     if (sf)
     {
-        osm_names <- names (x [[1]]) [which (grepl ('osm_', names (x [[1]])))]
-        core_names <- c ('osm_id', 'name', 'geometry')
-        for (i in osm_names)
-        {
-            xi <- lapply (x, function (j) j [[i]])
-            nrows <- lapply (xi, function (j) ifelse (is.null (j), 0, nrow (j)))
-            indx <- which (unlist (nrows) > 0)
-            xi <- xi [indx]
-            xi [vapply (xi, is.null, logical (1))] <- NULL
-            if (length (xi) > 0)
-            {
-                ids <- cnames <- NULL
-                for (j in xi)
-                {
-                    ids <- c (ids, rownames (j))
-                    cnames <- c (cnames, colnames (j))
-                }
-                ids <- sort (unique (ids))
-                cnames <- cnames [!cnames %in% core_names]
-                cnames <- sort (unique (cnames))
-                cnames <- c ('osm_id', 'name', cnames, 'geometry')
-                resi <- xi [[1]]
-                # then expand resi to final number of columns keeping sf
-                # integrity
-                cnames_new <- cnames [which (!cnames %in% names (resi))]
-                for (j in cnames_new)
-                    resi [j] <- rep (NA, nrow (resi))
-                # and re-order columns again
-                indx1 <- which (names (resi) %in% core_names)
-                indx2 <- which (!seq (ncol (resi)) %in% indx1)
-                indx <- c (which (names (resi) == 'osm_id'),
-                           which (names (resi) == 'name'),
-                           indx2 [order (names (resi) [indx2])],
-                           which (names (resi) == 'geometry'))
-                att <- attributes (resi)
-                resi <- resi [, indx, drop = FALSE]
-                nms <- names (resi)
-                attributes (resi) <- att # restored sf_column and agr attributes
-                names (resi) <- nms
-                # Then we're finally ready to pack in the remaining bits
-                xi [[1]] <- NULL
-                for (j in xi)
-                {
-                    rindx <- which (!rownames (j) %in% rownames (resi))
-                    # cindx <- which (names (j) %in% names (resi))
-                    resj <- j [rindx, , drop = FALSE] #nolint
-                    # then expand resj as for resi above
-                    cnames_new <- cnames [which (!cnames %in% names (resj))]
-                    for (k in cnames_new)
-                        resj [k] <- rep (NA, nrow (resj))
-                    indx1 <- which (names (resj) %in% core_names)
-                    indx2 <- which (!seq (ncol (resj)) %in% indx1)
-                    indx <- c (which (names (resj) == 'osm_id'),
-                               which (names (resj) == 'name'),
-                               indx2 [order (names (resj) [indx2])],
-                               which (names (resj) == 'geometry'))
-                    resj <- resj [, indx]
-                    resi <- rbind (resi, resj)
-                } # end for j in x
-                res [[i]] <- resi
-                attr (res [[i]], "sf_column") <- attr (resi, "sf_column")
-                attr (res [[i]], "agr") <- attr (resi, "agr")
-            } # end if length (xi) > 0
-        } # end for i in osm_names
-        class (res) <- c (class (res), "osmdata_sf")
+        res <- c_sf (res, x)
     } else
     {
         # TODO: implement sp version
@@ -179,5 +115,84 @@ c.osmdata <- function (...)
               "You could use\n'osmdata_sf()', and convert with ",
               "'as(x,'Spatial')' from package 'sf'.")
     }
+    return (res)
+}
+
+c_sf <- function (res, x) {
+
+    osm_names <- names (x [[1]]) [which (grepl ('osm_', names (x [[1]])))]
+    core_names <- c ('osm_id', 'name', 'geometry')
+
+    for (i in osm_names) {
+
+        xi <- lapply (x, function (j) j [[i]])
+        nrows <- lapply (xi, function (j) ifelse (is.null (j), 0, nrow (j)))
+        indx <- which (unlist (nrows) > 0)
+
+        xi <- xi [indx]
+        xi [vapply (xi, is.null, logical (1))] <- NULL
+
+        if (length (xi) > 0) {
+
+            ids <- cnames <- NULL
+            for (j in xi) {
+                ids <- c (ids, rownames (j))
+                cnames <- c (cnames, colnames (j))
+            }
+
+            ids <- sort (unique (ids))
+            cnames <- cnames [!cnames %in% core_names]
+            cnames <- sort (unique (c ('osm_id', 'name', cnames, 'geometry')))
+            resi <- xi [[1]]
+
+            # then expand resi to final number of columns keeping sf
+            # integrity
+            cnames_new <- cnames [which (!cnames %in% names (resi))]
+            for (j in cnames_new)
+                resi [j] <- rep (NA, nrow (resi))
+
+            # and re-order columns again
+            indx1 <- which (names (resi) %in% core_names)
+            indx2 <- which (!seq (ncol (resi)) %in% indx1)
+            indx <- c (which (names (resi) == 'osm_id'),
+                       which (names (resi) == 'name'),
+                       indx2 [order (names (resi) [indx2])],
+                       which (names (resi) == 'geometry'))
+            att <- attributes (resi)
+            resi <- resi [, indx, drop = FALSE]
+            nms <- names (resi)
+            attributes (resi) <- att # restored sf_column and agr attributes
+            names (resi) <- nms
+
+            # Then we're finally ready to pack in the remaining bits
+            xi [[1]] <- NULL
+            for (j in xi) {
+                rindx <- which (!rownames (j) %in% rownames (resi))
+                # cindx <- which (names (j) %in% names (resi))
+                resj <- j [rindx, , drop = FALSE] #nolint
+                # then expand resj as for resi above
+                cnames_new <- cnames [which (!cnames %in% names (resj))]
+                for (k in cnames_new)
+                    resj [k] <- rep (NA, nrow (resj))
+                indx1 <- which (names (resj) %in% core_names)
+                indx2 <- which (!seq (ncol (resj)) %in% indx1)
+                indx <- c (which (names (resj) == 'osm_id'),
+                           which (names (resj) == 'name'),
+                           indx2 [order (names (resj) [indx2])],
+                           which (names (resj) == 'geometry'))
+                resj <- resj [, indx]
+                resi <- rbind (resi, resj)
+            } # end for j in x
+
+            res [[i]] <- resi
+
+            attr (res [[i]], "sf_column") <- attr (resi, "sf_column")
+            attr (res [[i]], "agr") <- attr (resi, "agr")
+
+        } # end if length (xi) > 0
+    } # end for i in osm_names
+
+    class (res) <- c (class (res), "osmdata_sf")
+
     return (res)
 }
