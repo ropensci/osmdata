4304dfbb63c3ef9bbda00fb59a11d86da2e3a8ac
mpadge <mark.padgham@email.com>
2022-06-02 14:21:10 +0200

    add httptest2 redactor for #272

---
+++ inst/httptest2/redact.R
@@ -0,0 +1,44 @@
+function (resp) {
+
+    resp <- httptest2::gsub_response (
+        resp,
+        "https://nominatim.openstreetmap.org/",
+        "nominatim/",
+        fixed = TRUE
+    )
+
+    resp <- httptest2::gsub_response (
+        resp,
+        "https://wiki.openstreetmap.org/wiki/",
+        "wiki/",
+        fixed = TRUE
+    )
+
+    resp <- httptest2::gsub_response (
+        resp,
+        "overpass.kumi.systems/api/",
+        "overpass/",
+        fixed = TRUE
+    )
+
+    # Timestamp pattern:
+    ptn <- paste0 ("[A-Za-z]{3},\\s[0-9]{2}\\s[A-Za-z]{3}\\s[0-9]{4}\\s",   # date
+                   "[0-9]{2}\\:[0-9]{2}\\:[0-9]{2}")                        # time
+    resp <- httptest2::gsub_response (
+        resp,
+        ptn,
+        "Sat, 01 Jan 20222 00:00:00",
+        fixed = FALSE
+    )
+
+    # Datestamp pattern (for api status requests):
+    ptn <- "[0-9]{4}\\-[0-9]{2}\\-[0-9]{2}T[0-9]{2}\\:[0-9]{2}\\:[0-9]{2}Z"
+    resp <- httptest2::gsub_response (
+        resp,
+        ptn,
+        "2022-01-01T00:00:00Z",
+        fixed = FALSE
+    )
+
+    return (resp)
+}
