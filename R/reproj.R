c4c95f1b6d1a8c4b904ef701f233c036078c9bc9
mpadge <mark.padgham@email.com>
2022-06-09 10:57:40 +0200

    spaceout reproj.R

--- R/reproj.R
+++ R/reproj.R
@@ -2,12 +2,12 @@
 # testepsg -e "EPSG:4326"
 # testepsg -e "EPSG:3857"
 
-.sph_merc <- function() {
-  ## https://github.com/ropensci/mapscanner/issues/33
-  "+proj=merc +a=6378137 +b=6378137 +lat_ts=0 +lon_0=0 +x_0=0 +y_0=0 +k=1 +units=m +nadgrids=@null +wktext +no_defs"
+.sph_merc <- function () {
+    ## https://github.com/ropensci/mapscanner/issues/33
+    "+proj=merc +a=6378137 +b=6378137 +lat_ts=0 +lon_0=0 +x_0=0 +y_0=0 +k=1 +units=m +nadgrids=@null +wktext +no_defs"
 }
 
-.lonlat <- function() {
-  "+proj=longlat +datum=WGS84 +no_defs"
+.lonlat <- function () {
+    "+proj=longlat +datum=WGS84 +no_defs"
 
 }
