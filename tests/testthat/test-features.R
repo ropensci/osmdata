62df0098bbe7ce29310c6b6e09ba78f1e8440904
mpadge <mark.padgham@email.com>
2023-04-05 13:40:42 +0200

    update test-features; closes #322

--- tests/testthat/test-features.R
+++ tests/testthat/test-features.R
@@ -1,4 +1,3 @@
-
 # Note: the html files produced by these calls, and stored by `httptest2`, are
 # huge (> 1MB each), so have to be "post-processeed" here to reduce them to a
 # small sample only. Post-process of features is done in the test call; tags are
@@ -29,45 +28,62 @@ test_that ("available_features", {
 })
 
 
-post_process_tags <- function (dir_name, sample_index = 1:10, feature = NULL) {
+#' The raw HTML files are > 1MB. This function reduces their size by selecting
+#' just a few sample nodes, and only tables for a specific key. Note that keys
+#' like "highway" still correspond to overly large tables. The default key
+#' corresponds to the item in `tables_r` below which has the smallest number of
+#' rows.
+#' @noRd
+post_process_tags <- function (dir_name, keys = "building") {
 
     fname <- list.files (dir_name,
         full.names = TRUE,
         recursive = TRUE
     ) [1]
+
     x <- xml2::read_html (fname)
-    nodes_all <- rvest::html_nodes (x, "div[class='taglist']")
-    if (!is.null (feature)) {
-        # see features.R/available_tags() for details:
-        nodes_sample <- rvest::html_nodes (
-            x,
-            sprintf ("a[title^='Tag:%s']", feature)
-        )
-    } else {
-        nodes_sample <- nodes_all [sample_index]
-    }
-    writeLines (as.character (nodes_sample), fname)
+
+    nodes_sample <- rvest::html_nodes (x, "div[class='taglist']") [1:10]
+
+    tables_sample <- rvest::html_nodes (x, "table")
+    tables_r <- rvest::html_table (tables_sample)
+    index <- which (vapply (tables_r, function (i) {
+        ret <- FALSE
+        if ("Key" %in% names (i)) {
+            ret <- any (keys %in% i$Key)
+        }
+        return (ret)
+    }, logical (1L)))
+    tables_sample <- tables_sample [index]
+
+    xsample <- c (as.character (nodes_sample), as.character (tables_sample))
+
+    writeLines (xsample, fname)
 }
 
 test_that ("available_tags", {
 
-    expect_error (available_tags ("highway", 1), "unused argument")
+    expect_error (available_tags ("building", 1), "unused argument")
     post_process <- !dir.exists ("mock_tags_fail")
-    tags <- with_mock_dir ("mock_tags_fail", {
-        available_tags ("junk")
-    })
+    expect_error (
+        tags <- with_mock_dir ("mock_tags_fail", {
+            available_tags ("junk")
+        }),
+        "feature \\[junk\\] not listed as Key"
+    )
     if (post_process) {
-        post_process_tags ("mock_tags_fail", 1:10)
+        post_process_tags ("mock_tags_fail")
     }
-    expect_length (tags, 0L)
 
     post_process <- !dir.exists ("mock_tags")
     tags <- with_mock_dir ("mock_tags", {
-        available_tags ("highway")
+        available_tags ("building")
     })
     if (post_process) {
-        post_process_tags ("mock_tags", feature = "highway")
+        post_process_tags ("mock_tags", keys = "building")
     }
-    expect_is (tags, "character")
-    expect_true (length (tags) > 1L)
+    expect_s3_class (tags, "data.frame")
+    expect_equal (names (tags), c ("Key", "Value"))
+    expect_true (all (tags$Key == "building"))
+    expect_true (nrow (tags) > 1L)
 })
